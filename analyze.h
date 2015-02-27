b5e9971c8d927a48e4f2d43dfa8d7c11ea42cf28
saito <sutagiro@qanzoo.sakura.ne.jp>
2015-02-27 09:23:01 +0900

    add analyze.h normalize.h

---
+++ analyze.h
@@ -0,0 +1,7 @@
+#ifndef ANALYZE_H
+#define ANALYZE_H
+#include "numpl.h"
+#include "solve.h"
+double analyze(numpl_array * array, solve_info * info);
+double get_analyze_value(solve_info * info);
+#endif // ANALYZE_H
---
+++ normalize.h
@@ -0,0 +1,17 @@
+#ifndef NORMALIZE_H
+#define NORMALIZE_H
+
+#include "normalize.h"
+#include "analyze.h"
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+    int lazy_normalize(numpl_array * array, solve_info * info);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif // NORMALIZE6X6_H
